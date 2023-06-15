d2533cc433186093d21f39fcc9a1e9d49dc7831b
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2023-06-15 14:33:41 -0500

    new changes

---
+++ basic_structure_form_one/assets/js/javaScript.js
@@ -0,0 +1,23 @@
+/*COMENTARIOS*/
+//COMENTARIOS
+
+//VARIABLES
+//var variable global
+//let variable solo en un bloque de código
+//const variable que mantiene un valor constante
+
+//STRING
+
+//NUMBER
+
+//BOOLEAN
+
+//ARRAY 
+
+//OBJECT
+
+
+//CONDCIONALE
+//CICLOS
+
+console.log("Hola Mundo");
