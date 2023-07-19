7cf9fdd082f58bda6cf32ff8046e13b3dbf056cf
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2023-07-19 18:23:50 -0500

    new changes
    Changes get data  user

--- basic_structure_form_one/view/user/edit.php
+++ basic_structure_form_one/view/user/edit.php
@@ -1,37 +1,43 @@
 <?php
 include("../../config/config.php");
 
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  $userId = $_REQUEST['User_id'];
+  $sql = "CALL sp_select_user_id(" . $userId . "); ";
+  $sql .= "SELECT * FROM `document_type` WHERE 1;";
+  $sql .= "SELECT * FROM `gendertype` WHERE 1;";
+  $sql .= "SELECT * FROM `status` WHERE 1;";
+  $resultArray = array();
+  if (!$connect->multi_query($sql)) {
+    echo "Falló la multiconsulta: (" . $connect->errno . ") " . $connect->error;
+  }
 
-$sql = "CALL sp_select_all_user(); ";
-$sql .= "SELECT * FROM `document_type` WHERE 1;";
-$sql .= "SELECT * FROM `gendertype` WHERE 1;";
-$sql .= "SELECT * FROM `status` WHERE 1;";
-$resultArray = array();
-if (!$connect->multi_query($sql)) {
-  echo "Falló la multiconsulta: (" . $connect->errno . ") " . $connect->error;
+  do {
+    if ($result = $connect->store_result()) {
+
+
+      $resultQuery = $result->fetch_all(MYSQLI_NUM);
+      array_push($resultArray, $resultQuery);
+
+      $result->free();
+    }
+  } while ($connect->more_results() && $connect->next_result());
+  $resultUser = $resultArray[0];
+  $resultDocumentType = $resultArray[1];
+  $resultGenderType = $resultArray[2];
+  $resultStatus = $resultArray[3];
 }
 
-do {
-  if ($resultado = $connect->store_result()) {
 
 
-    $result = $resultado->fetch_all(MYSQLI_NUM);
-    array_push($resultArray, $result);
 
-    $resultado->free();
-  }
-} while ($connect->more_results() && $connect->next_result());
-$resultUser = $resultArray[0];
-$resultDocumentType = $resultArray[1];
-$resultGenderType = $resultArray[2];
-$resultStatus = $resultArray[3];
 
 ?>
 <!-- 
   #Ahutor:DIEGO CASALLAS
   #Busines: 
-  #Date:14/07/2023
-  #Description:View user
+  #Date:19/07/2023
+  #Description:View update user
   #Vesion:1.0
 -->
 <!--<!DOCTYPE html> En primer lugar necesitamos indicar el tipo de documento que estamos creando. Esto en HTML5 es extremadamente sencillo: -->
@@ -62,7 +68,7 @@ $resultStatus = $resultArray[3];
   <!--Etiqueta div - class => permite a una clase de css acceder a su contenido <div>-->
   <div id="sectionOne" class="sectionOne" name="sectionOne">
     <!--Etiqueta h2 de titulo <h2>-->
-    <h2>REGISTRAR USUARIOS</h2>
+    <h2>ACTUALIZAR USUARIOS</h2>
 
     <!--Inicio de formulario - etiqueta de formulario <form>-->
     <!--Formulario form - name => permite a un script acceder a su contenido <input>-->
@@ -77,7 +83,7 @@ $resultStatus = $resultArray[3];
       <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
       <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
       <!--Caja de texto etiqueta input - type => hidden campo oculto en el html<input>-->
-      <input type="hidden" value="" id="User_id" name="User_id" />
+      <input type="hidden" value="<?= $resultUser[0][0] ?>" id="User_id" name="User_id" />
       <table name="tableUser" id="tableUser" class="tableUser">
         <!--Inicio de fila para de la tabla - etiqueta de fila <tr>-->
         <tr>
@@ -90,7 +96,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="text" value="" placeholder="Digitar Nombre" id="User_name" name="User_name" required />
+            <input type="text" value="<?= $resultUser[0][1] ?>" placeholder="Digitar Nombre" id="User_name" name="User_name" required />
           </td>
           <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
 
@@ -103,7 +109,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="text" value="" placeholder="Digitar Apellido" id="User_lastName" name="User_lastName" required />
+            <input type="text" value="<?= $resultUser[0][2] ?>" placeholder="Digitar Apellido" id="User_lastName" name="User_lastName" required />
           </td>
           <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
           <!--Inicio de columna para de la tabla - etiqueta de columna <td>-->
@@ -115,7 +121,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="number" value="" placeholder="Digitar Documento" id="User_document" name="User_document" required />
+            <input type="number" value="<?= $resultUser[0][4] ?>" placeholder="Digitar Documento" id="User_document" name="User_document" required disabled />
 
           </td>
 
@@ -133,7 +139,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="email" value="" placeholder="Digitar Correo Electrónico" id="User_email" name="User_email" required />
+            <input type="email" value="<?= $resultUser[0][5] ?>" placeholder="Digitar Correo Electrónico" id="User_email" name="User_email" required />
           </td>
           <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
 
@@ -146,7 +152,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="number" value="" placeholder="Digitar Número de Celular" id="User_cellphone" name="User_cellphone" required />
+            <input type="number" value="<?= $resultUser[0][6] ?>" placeholder="Digitar Número de Celular" id="User_cellphone" name="User_cellphone" required />
           </td>
           <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
           <!--Inicio de columna para de la tabla - etiqueta de columna <td>-->
@@ -158,7 +164,7 @@ $resultStatus = $resultArray[3];
             <!--Caja de texto etiqueta input - name => permite a un script acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - id => permite a un script o al css acceder a su contenido <input>-->
             <!--Caja de texto etiqueta input - class => permite a una clase de css acceder a su contenido <input>-->
-            <input type="date" value="" placeholder="Fecha de Nacimiento" id="birthdate" name="birthdate" required />
+            <input type="date" value="<?= $resultUser[0][9] ?>" placeholder="Fecha de Nacimiento" id="birthdate" name="birthdate" required />
           </td>
           <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
 
@@ -179,8 +185,13 @@ $resultStatus = $resultArray[3];
               <!--Inicio de etiqueta de opciones de lista <option>-->
               <!--Valor de la opción - value => valor de la opción <option>-->
               <?php
+
               for ($i = 0; $i < count($resultDocumentType); $i++) {
-                echo '<option value="' . $resultDocumentType[$i][0] . '">' . $resultDocumentType[$i][1] . '</option>';
+                if ($resultUser[0][11] == $resultDocumentType[$i][0]) {
+                  echo '<option value="' . $resultDocumentType[$i][0] . '" selected="selected">' . $resultDocumentType[$i][1] . '</option>';
+                } else {
+                  echo '<option value="' . $resultDocumentType[$i][0] . '">' . $resultDocumentType[$i][1] . '</option>';
+                }
               };
               ?>
             </select>
@@ -201,7 +212,13 @@ $resultStatus = $resultArray[3];
               <!--Valor de la opción - value => valor de la opción <option>-->
               <?php
               for ($i = 0; $i < count($resultGenderType); $i++) {
-                echo '<option value="' . $resultGenderType[$i][0] . '">' . $resultGenderType[$i][1] . '</option>';
+
+
+                if ($resultUser[0][12] == $resultGenderType[$i][0]) {
+                  echo '<option value="' . $resultGenderType[$i][0] . '" selected="selected">' . $resultGenderType[$i][1] . '</option>';
+                } else {
+                  echo '<option value="' . $resultGenderType[$i][0] . '">' . $resultGenderType[$i][1] . '</option>';
+                }
               };
 
               ?>
@@ -225,7 +242,12 @@ $resultStatus = $resultArray[3];
               <!--Valor de la opción - value => valor de la opción <option>-->
               <?php
               for ($i = 0; $i < count($resultStatus); $i++) {
-                echo '<option value="' . $resultStatus[$i][0] . '">' . $resultStatus[$i][1] . '</option>';
+
+                if ($resultUser[0][13] == $resultStatus[$i][0]) {
+                  echo '<option value="' . $resultStatus[$i][0] . '" selected="selected">' . $resultStatus[$i][1] . '</option>';
+                } else {
+                  echo '<option value="' . $resultStatus[$i][0] . '">' . $resultStatus[$i][1] . '</option>';
+                }
               };
               ?>
             </select>
@@ -313,138 +335,7 @@ $resultStatus = $resultArray[3];
   <!--Cierre de división etiqueta </div>-->
 
   <!--*****************************************************************************************************************-->
-  <!--Inicio de división etiqueta <div>-->
-  <!--Etiqueta div - id => permite a un script o al css acceder a su contenido <div>-->
-  <!--Etiqueta div - class => permite a una clase de css acceder a su contenido <div>-->
-  <div id="sectionTwo" class="sectionTwo">
-    <!--Etiqueta h1 de titulo <h1>-->
-    <h2 style="text-align: center;">LISTA DE USUARIOS</h2>
-    <div>
-      <form>
-        <table name="tableViewUserSearch" id="tableViewUserSearch" class="tableViewUserSearch">
-          <tr>
-            <!--Inicio de columna para de la tabla - etiqueta de columna <td>-->
-            <td>
-              <!--La etiqueta select (<select>) de HTML representa un control que muestra un menú de opciones-->
-              <!--Inicio de la lista <select>-->
-              <!--lista de selección select - required => indica que la caja de texto es requerida (obligatoria) <select>-->
-              <!--lista de selección select - name => permite a un script acceder a su contenido <select>-->
-              <!--lista de selección select - id => permite a un script o al css acceder a su contenido <select>-->
-              <!--lista de selección select - class => permite a una clase de css acceder a su contenido <select>-->
-              <select name="typeSearch" id="typeSearch" required>
-                <!--Inicio de etiqueta de opciones de lista <option>-->
-                <!--Valor de la opción - value => valor de la opción <option>-->
 
-                <option value="0">DOCUMENTO</option>
-                <!--Cierre de etiqueta de opciones de lista <option>-->
-                <!--Inicio de etiqueta de opciones de lista <option>-->
-                <!--Valor de la opción - value => valor de la opción <option>-->
-                <option value="1">NOMBRE</option>
-                <!--Cierre de etiqueta de opciones de lista <option>-->
-                <!--Inicio de etiqueta de opciones de lista <option>-->
-                <!--Valor de la opción - value => valor de la opción <option>-->
-                <option value="2">APELLIDO</option>
-                <!--Cierre de etiqueta de opciones de lista <option>-->
-                <!--Cierre de etiqueta de opciones de lista <option>-->
-                <!--Inicio de etiqueta de opciones de lista <option>-->
-                <!--Valor de la opción - value => valor de la opción <option>-->
-                <option value="3">CORREO</option>
-                <!--Cierre de etiqueta de opciones de lista <option>-->
-              </select>
-              <!--Cierre de la lista <select>-->
-            </td>
-            <!--Cierre de columna para de la tabla - etiqueta de columna <td>-->
-            <td> <input type="search" value="" placeholder="Digitar busqueda" required /> </td>
-            <td style="text-align: center;"> <button> <img src="../../assets/img/icons/search.png" /> </button></td>
-          </tr>
-
-        </table>
-      </form>
-    </div>
-
-    <!--Inicio de tabla para los contenidos del formulario - etiqueta de tabla <table>-->
-    <!--Etiqueta table - name => permite a un script acceder a su contenido <table>-->
-    <!--Etiqueta table - id => permite a un script o al css acceder a su contenido <div>-->
-    <!--Etiqueta table - class => permite a una clase de css acceder a su contenido <div>-->
-    <table name="tableViewUser" id="tableViewUser" class="tableViewUser">
-      <!--Inicio de cabeza de tabla para los contenidos de la tabla  - etiqueta de cabeza tabla <thead>-->
-
-      <thead>
-        <tr>
-
-          <!--Inicio de las columnas de la tabla de la cabecera  <th>-->
-          <th>#</th>
-          <th>NOMBRE</th>
-          <th>APELLIDO</th>
-          <th>TIPO DE DOCUMENTO</th>
-          <th>DOCUMENTO</th>
-          <th>CORREO</th>
-          <th>CELULAR</th>
-          <th>CONTRASEÑA</th>
-          <th>GÉNERO</th>
-          <th>FECHA DE NACIMIENTO</th>
-          <th>ESTADO</th>
-          <th>ACCIONES</th>
-
-        </tr>
-      </thead>
-      <!--Cierre de cabeza de tabla para los contenidos de la tabla  - etiqueta de cabeza tabla <thead>-->
-      <!--Inicio de cuerpo de tabla para los contenidos de la tabla  - etiqueta de cabeza tabla <tbody>-->
-
-      <tbody>
-        <?php
-        $rowUser = $resultUser;
-        for ($i = 0; $i < count($rowUser); $i++) {
-          echo '<tr class="checkTr">';
-          echo '<td>' . ($i + 1) . '</td>';
-          echo '<td>' . $rowUser[$i][1] . '</td>';
-          echo '<td>' . $rowUser[$i][2] . '</td>';
-          echo '<td>' . $rowUser[$i][3] . '</td>';
-          echo '<td>' . $rowUser[$i][4] . '</td>';
-          echo '<td>' . $rowUser[$i][5] . '</td>';
-          echo '<td>' . $rowUser[$i][6] . '</td>';
-          echo '<td>' . $rowUser[$i][7] . '</td>';
-          echo '<td>' . $rowUser[$i][8] . '</td>';
-          echo '<td>' . $rowUser[$i][9] . '</td>';
-          echo '<td>' . $rowUser[$i][10] . '</td>';
-          echo '
-          <td class="btnsActions" style="text-align: center;">
-            <button class="btnView"> <img src="../../assets/img/icons/visibility.png" /> </button>
-            <button class="btnUpdate"> <img src="../../assets/img/icons/edit.png" /> </button>
-            <button class="btnDelete"> <img src="../../assets/img/icons/remove.png" /> </button>
-          </td>
-        </tr>';
-        };
-
-        $connect->close();
-        ?>
-      </tbody>
-      <!--Cierre de cuerpo de tabla para los contenidos de la tabla  - etiqueta de cabeza tabla <tbody>-->
-      <!--Inicio de pie de tabla para los contenidos de la tabla  - etiqueta de pie tabla <tfoot>-->
-      <tfoot>
-        <tr>
-          <th>#</th>
-          <th>NOMBRE</th>
-          <th>APELLIDO</th>
-          <th>TIPO DE DOCUMENTO</th>
-          <th>DOCUMENTO</th>
-          <th>CORREO</th>
-          <th>CELULAR</th>
-          <th>CONTRASEÑA</th>
-          <th>GÉNERO</th>
-          <th>FECHA DE NACIMIENTO</th>
-          <th>ESTADO</th>
-          <th>ACCIONES</th>
-        </tr>
-
-      </tfoot>
-      <!--Cierre de pie de tabla para los contenidos de la tabla  - etiqueta de pie tabla <tfoot>-->
-    </table>
-    <!--Cierre de tabla para los contenidos del formulario - etiqueta de tabla <table>-->
-
-  </div>
-  <!--Cierre de división etiqueta </div>-->
-  <!--*****************************************************************************************************************-->
   <!--Etiqueta de enlace con la logíca de javascript <script>-->
   <script src="../assets/js/main.js" type="javascript"></script>
 </body>
