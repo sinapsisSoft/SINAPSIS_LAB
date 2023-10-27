deab2140d52be9c08c6ad37b11d0c421f360028d
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2023-10-27 08:05:39 -0500

    Update report.php

--- structure_project_mvc/app/Views/report/report.php
+++ structure_project_mvc/app/Views/report/report.php
@@ -33,3 +33,7 @@ include_once('../app/Views/template/header.php');
 include_once('../app/Views/template/footer.php');
 ?>
 <!-- Container Jscript-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="../assets/js/constans.js"> </script>
+  <script src="../controllers/home/controllerHome.js"> </script>
